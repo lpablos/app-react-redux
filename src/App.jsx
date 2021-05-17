89be7bd6eaf70da3951289adac6bf64913a72dd0
Luis Jorge <gevolutionxalapa@gmail.com>
2021-05-17 08:39:40 -0500

    Agrege componentes de navar y login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Pokemones from './componentes/Pokemones'
+import Login from './componentes/Login'
+import Navbar from './componentes/Navbar';
 
 import {
   BrowserRouter as Router,
@@ -12,8 +14,10 @@ function App() {
   return (
     <Router>
       <div className="container mt-3">
+        <Navbar/>
         <Switch>
           <Route component={Pokemones} path="/" exact/>
+          <Route component={Login} path="/login" exact/>          
         </Switch>
       </div>
     </Router>
---
+++ src/componentes/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <h3>Area de Login</h3>
+        </div>
+    )
+}
+
+export default Login
